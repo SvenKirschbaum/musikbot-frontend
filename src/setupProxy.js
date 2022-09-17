be89da72afc915f88eaa406ce50d467ede5768e2
Sven Kirschbaum <sven@kirschbaum.me>
2022-09-17 23:54:36 +0200

    :hammer: Change proxy order to fix websocket issue in development

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,24 +1,24 @@
 // noinspection NpmUsedModulesInstalled
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
-const host = 'https://musikbot.elite12.de';
-// const host = 'http://localhost:8080';
+// const host = 'https://musikbot.elite12.de';
+const host = 'http://localhost:8080';
 const secure = host.includes('https');
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: host,
-            secure,
-            changeOrigin: secure,
-        })
-    );
     app.use(
         '/api/sock',
         createProxyMiddleware({
             target: host,
             ws: true,
+            secure,
+            changeOrigin: secure,
+        })
+    );
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: host,
             secure,
             changeOrigin: secure,
         })
