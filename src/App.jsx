e1b0562f75c60c1af03a64517e0568f031b786a9
Sven Kirschbaum <sven@kirschbaum.me>
2024-07-13 22:17:20 +0200

    fix: Delay rendering while authentication is in progress

--- src/App.jsx
+++ src/App.jsx
@@ -131,6 +131,8 @@ function AppRouter() {
         }
     }, [auth?.user?.access_token]);
 
+    if (auth.isLoading) return (<div>Loading...</div>);
+
     return (
         <StompSessionProvider
             url={`/api/sock/`}
